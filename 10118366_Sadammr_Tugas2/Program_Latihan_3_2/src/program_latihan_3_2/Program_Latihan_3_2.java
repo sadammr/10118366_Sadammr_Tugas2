90e304c0a428437aa755d88587368d8b25d6b3e8
sadammr <62872095+sadammr@users.noreply.github.com>
2020-04-09 01:20:10 +0700

    Add files via upload

---
+++ 10118366_Sadammr_Tugas2/Program_Latihan_3_2/src/program_latihan_3_2/Program_Latihan_3_2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package program_latihan_3_2;
+
+/**
+ *
+ * @author Sadam
+ */
+public class Program_Latihan_3_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
